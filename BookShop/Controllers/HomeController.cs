2f2bb15b4118cfc0c34a205a716c345b6718a300
MIJU <MIJU@DESKTOP-68GPOKE>
2024-11-20 21:55:28 +0600

    add mvc

---
+++ BookShop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Controllers
+{
+    public class HomeController: Controller
+    {
+        public string Index()
+        {
+            return "Home Page Message";
+        }
+    }
+}
